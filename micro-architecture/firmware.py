88d486c5c8dea7e1d1d862dac52971c9341df4fd
Luiz Gustavo Marques Costa <537585@UCS.DC.UFC.BR>
2023-06-21 09:31:25 -0300

    Save working changes 2

--- micro-architecture/firmware.py
+++ micro-architecture/firmware.py
@@ -21,32 +21,47 @@ class Firmware:
 
 firmware = Firmware()
 
+# main fetch; goto MBR1
+firmware.set_instruction(0, 0b000000000_100_00000000_00000000_000_000_001)
 # HALT
 firmware.set_instruction(255, 0b000000000_000_00000000_00000000_000_000_000)
 
 # X1 = X1 + memory[address]
 
 # 2: MAR <- MBR1; MDR <- read_word(MAR); goto 3
-firmware.set_instruction(2, 0b000000011_000_00010100_10000000_000_011_010)
+firmware.set_instruction(2, 0b000000011_000_00010100_10000000_000_010_010)
 
 # 3: X1 <- MDR + X1; goto MBR1
-firmware.set_instruction(3, 0b000000000_100_00111100_00010000_000_100_001)
+firmware.set_instruction(3, 0b000000000_100_00111100_00010000_001_100_001)
 
 # X1 = X1 - memory[address]
 
-# 2: MAR <- MBR1; MDR <- read_word(MAR); goto 3
-firmware.set_instruction(4, 0b000000101_000_00010100_10000000_000_011_010)
+# 2: MAR <- MBR1; MDR <- read_word(MAR); goto 4
+firmware.set_instruction(4, 0b000000101_000_00010100_10000000_000_010_010)
 
 # 3: X1 <- X1 - MDR; goto MBR1
-firmware.set_instruction(5, 0b000000000_100_00111111_00010000_000_100_001)
+firmware.set_instruction(5, 0b000000000_100_00111111_00010000_001_100_001)
 
 # memory[address] = X1
 
 # MAR <- MBR1; goto 7
-firmware.set_instruction(6, 0b000000111_000_00010100_10000000_000_011_000)
+firmware.set_instruction(6, 0b000000111_000_00010100_10000000_000_010_000)
 
 # MDR <- X1; write; goto MBR1
-firmware.set_instruction(7, 0b000000000_000_00010100_01000000_000_011_100)
+firmware.set_instruction(7, 0b000000000_100_00010100_01000000_000_100_100)
+
+# goto address 
+# PC = 0
+# PC <- PC + 1
+# MBR1 = read_word(PC+1)
+# PC <- MBR1
+# MBR1 <- read_word(PC)
+# goto MBR1
+
+## 14: PC <- MBR1; fetch; goto MBR1
+firmware.set_instruction(8, 0b000000000_100_00010100_00100000_000_010_001)
+
+# if X == 0 goto address
 
 """ # main: PC <- PC + 1; MBR <- read_byte(PC); goto MBR
 firmware.set_instruction(0, 0b000000000_100_00110101_001000_001_001) 
