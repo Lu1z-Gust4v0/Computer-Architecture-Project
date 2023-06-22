c528f476c03fdd1657089e3a619b473830daea5f
Gustavo Costa <gustavo1204.gc@gmail.com>
2023-06-22 10:05:53 -0300

    Save working changes

--- micro-architecture/computer.py
+++ micro-architecture/computer.py
@@ -2,7 +2,22 @@ from cpu import cpu
 from memory import memory
 from clock import clock
 
-memory.write_word(50, 21) 
+# memory[50] = 2
+memory.write_word(50, 2)
+# memory[100] = 2
+memory.write_word(100, 2)
+# X1 <- X1 + memory[50]
+memory.write_byte(0, 1)
+memory.write_byte(1, 50)
+# X1 <- X1 + memory[100]
+memory.write_byte(2, 1)
+memory.write_byte(3, 100)
+
+clock.start(cpu)
+
+print(cpu.registers["X1"])
+
+""" memory.write_word(50, 21) 
 memory.write_word(100, 32)
 memory.write_word(130, 10)
 
@@ -28,8 +43,8 @@ memory.write_byte(10, 150)
     
 # stop
 memory.write_byte(11, 255)
-
+ """
 
 clock.start(cpu)
 
-print(memory.read_word(150))
+# print(memory.read_word(150))
