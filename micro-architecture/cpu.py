174b3b7a4729b4d32bcdc03c20146832a807c2e6
Gustavo Costa <gustavo1204.gc@gmail.com>
2023-06-24 12:32:28 -0300

    Format cpu code

--- micro-architecture/cpu.py
+++ micro-architecture/cpu.py
@@ -4,24 +4,24 @@ from IFU import ifu
 
 alu_operations = {
     0b000000: lambda A, B: 0,
-    0b011000: lambda A, B: A, 
+    0b011000: lambda A, B: A,
     0b010100: lambda A, B: B,
-    0b011010: lambda A, B: ~A, 
-    0b101100: lambda A, B: ~B, 
-    0b111100: lambda A, B: A + B, 
-    0b111101: lambda A, B: A + B + 1, 
-    0b111001: lambda A, B: A + 1, 
+    0b011010: lambda A, B: ~A,
+    0b101100: lambda A, B: ~B,
+    0b111100: lambda A, B: A + B,
+    0b111101: lambda A, B: A + B + 1,
+    0b111001: lambda A, B: A + 1,
     0b110101: lambda A, B: B + 1,
-    0b111111: lambda A, B: B - A, 
-    0b110110: lambda A, B: B - 1, 
+    0b111111: lambda A, B: B - A,
+    0b110110: lambda A, B: B - 1,
     0b111011: lambda A, B: -A,
-    0b001100: lambda A, B: A & B, 
-    0b011100: lambda A, B: A | B, 
-    0b010000: lambda A, B: 0, 
-    0b110001: lambda A, B: 1, 
-    0b110010: lambda A, B: -1, 
+    0b001100: lambda A, B: A & B,
+    0b011100: lambda A, B: A | B,
+    0b010000: lambda A, B: 0,
+    0b110001: lambda A, B: 1,
+    0b110010: lambda A, B: -1,
 }
-                                                                                                                 
+
 shifts = {
     0b00: lambda A: A,
     0b01: lambda A: A << 1,
@@ -29,7 +29,7 @@ shifts = {
     0b11: lambda A: A << 8,
 }
 
-# next address | jmp | alu | C | write read fetch | A | B | 
+# next address | jmp | alu | C | write read fetch | A | B |
 # 000000000_000_00000000_00000000_000_000_000
 #     9      3     8        8      3   3   3
 
@@ -47,7 +47,7 @@ registers = {
     "X1": 0,
     "X2": 0,
     "X3": 0,
-    "H": 0, 
+    "H": 0,
 
     # Alu's output registers
     "N": 0,
@@ -57,7 +57,7 @@ registers = {
 reg_code = {
     0: "MAR",
     1: "MDR",
-    2: "MBR1", 
+    2: "MBR1",
     3: "MBR2",
     4: "X1",
     5: "X2",
@@ -65,8 +65,15 @@ reg_code = {
     7: "H"
 }
 
-class CPU: 
-    def __init__(self, registers=registers, memory=memory, firmware=firmware, ifu=ifu):
+
+class CPU:
+    def __init__(
+        self,
+        registers=registers,
+        memory=memory,
+        firmware=firmware,
+        ifu=ifu
+    ):
         self.memory = memory
         self.registers = registers
         self.firmware = firmware
@@ -80,26 +87,26 @@ class CPU:
         self.ifu.consume_mbr1()
         self.ifu.load(self)
 
-        self.registers["PC"] += 1 
-        
+        self.registers["PC"] += 1
+
         return mbr1
 
     def read_mbr2(self):
         mbr2 = self.registers["MBR2"]
         self.ifu.consume_mbr2()
         self.ifu.load(self)
-        
+
         self.registers["PC"] += 2
 
         return mbr2
 
     def fetch_word(self):
         self.ifu.fetch(self.memory)
-        self.ifu.load(self) 
+        self.ifu.load(self)
 
     def update_pc(self, value):
         # print(f"PC IS BEING UPDATED {value}")
-        self.registers["PC"] = value 
+        self.registers["PC"] = value
         self.ifu.update_imar(value)
         # print(f"IMAR VALUE {self.ifu.IMAR}")
 
@@ -107,7 +114,7 @@ class CPU:
     def read_regs(self, mir):
         register_a = (mir & 0b111000) >> 3
         register_b = mir & 0b111
-        
+
         # self.ifu.shift_register.print_queue()
         # print("length", self.ifu.shift_register.length)
         # print(f"A: {register_a} B: {register_b}")
@@ -115,7 +122,7 @@ class CPU:
         self.BUS_A = self.registers[reg_code[register_a]]
         self.BUS_B = self.registers[reg_code[register_b]]
 
-        # Consume MBRs 
+        # Consume MBRs
         if reg_code[register_a] == "MBR1":
             self.BUS_A = self.read_mbr1()
 
@@ -134,16 +141,16 @@ class CPU:
 
         if write_bits & 0b10000000:
             self.registers["MAR"] = self.BUS_C
-            
+
         if write_bits & 0b01000000:
             self.registers["MDR"] = self.BUS_C
-            
+
         if write_bits & 0b00100000:
             self.update_pc(self.BUS_C)
 
         if write_bits & 0b00010000:
             self.registers["X1"] = self.BUS_C
-            
+
         if write_bits & 0b00001000:
             self.registers["X2"] = self.BUS_C
 
@@ -157,16 +164,16 @@ class CPU:
     def alu(self, mit):
         control_bits = (mit >> 17) & 0b11111111
 
-        INPUT_A = self.BUS_A 
+        INPUT_A = self.BUS_A
         INPUT_B = self.BUS_B
         OUTPUT = 0
-        
-        shift_bits = (control_bits & 0b11000000) >> 6        
+
+        shift_bits = (control_bits & 0b11000000) >> 6
         operation_bits = control_bits & 0b00111111
 
         OUTPUT = alu_operations[operation_bits](INPUT_A, INPUT_B)
-            
-        print("ALU output", OUTPUT)
+
+        # print("ALU output", OUTPUT)
 
         if OUTPUT == 0:
             self.registers["N"] = 0
@@ -174,9 +181,9 @@ class CPU:
         else:
             self.registers["N"] = 1
             self.registers["Z"] = 0
-            
+
         OUTPUT = shifts[shift_bits](OUTPUT)
-            
+
         self.BUS_C = OUTPUT
 
     # Calculate the next instruction
@@ -189,37 +196,44 @@ class CPU:
             return
         if jam & 0b001:                 # JAMZ
             next_instruction = next_instruction | (self.registers["Z"] << 8)
-            
+
         if jam & 0b010:                 # JAMN
             next_instruction = next_instruction | (self.registers["N"] << 8)
 
         if jam & 0b100:                 # JMPC
             next_instruction = next_instruction | self.read_mbr1()
-        
+
         self.registers["MPC"] = next_instruction
 
     # Operations with memory
     def memory_io(self, mir):
         memory_bits = (mir & 0b111000000) >> 6
-        
+
         if memory_bits & 0b001:                # FETCH
             self.fetch_word()
-        
+
         if memory_bits & 0b010:                # READ
-            self.registers["MDR"] = self.memory.read_word(self.registers["MAR"])
-        
+            self.registers["MDR"] = self.memory.read_word(
+                self.registers["MAR"]
+            )
+
         if memory_bits & 0b100:                # WRITE
-           self.memory.write_word(self.registers["MAR"], self.registers["MDR"])
+            self.memory.write_word(
+                self.registers["MAR"],
+                self.registers["MDR"]
+            )
 
     # Emulates one cpu 'step'
     def step(self):
-        self.registers["MIR"] = self.firmware.get_instruction(self.registers["MPC"])
+        self.registers["MIR"] = self.firmware.get_instruction(
+            self.registers["MPC"]
+        )
         # print("instruction:", bin(self.registers["MIR"]))
         # print("state before", self.registers)
 
         if self.registers["MIR"] == 0:
-            return False    
-        
+            return False
+
         self.read_regs(self.registers["MIR"])
         self.alu(self.registers["MIR"])
         self.write_regs(self.registers["MIR"])
@@ -230,5 +244,6 @@ class CPU:
         # print()
 
         return True
+
 
 cpu = CPU()
