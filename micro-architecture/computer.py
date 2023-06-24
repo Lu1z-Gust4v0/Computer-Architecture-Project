9df74b6c69f567d1235b3363de677d1b85479683
Gustavo Costa <gustavo1204.gc@gmail.com>
2023-06-24 12:34:55 -0300

    Add if x1 == 0 goto instructions Fix write to memory instruction

--- micro-architecture/computer.py
+++ micro-architecture/computer.py
@@ -2,65 +2,73 @@ from cpu import cpu
 from memory import memory
 from clock import clock
 
-# memory[50] = 2
-memory.write_word(50, 22)
-# memory[100] = 2
-memory.write_word(100, 13)
-# X1 <- X1 + memory[50]
-memory.write_byte(0, 1)
-memory.write_byte(1, 50)
-# X1 <- X1 + memory[100]
-memory.write_byte(2, 3)
-memory.write_byte(3, 100)
-# memory[150] = X1
-memory.write_byte(4, 5)
-memory.write_byte(5, 150)
-# goto 12
-memory.write_byte(6, 7)
-memory.write_byte(7, 12)
-# X1 <- X1 - memory[150]
-memory.write_byte(8, 0)
-memory.write_byte(9, 0)
-# X1 <- X1 - memory[150]
-memory.write_byte(10, 0)
-memory.write_byte(11, 0)
+# # memory[50] = 2
+# memory.write_word(50, 2)
+# # memory[100] = 2
+# memory.write_word(100, 2)
+# # memory[77] = 77
+# memory.write_word(77, 77)
+# # X1 <- X1 + memory[50]
+# memory.write_byte(0, 1)
+# memory.write_byte(1, 50)
+# # X1 <- X1 - memory[100]
+# memory.write_byte(2, 3)
+# memory.write_byte(3, 100)
+# # if X1 == 0 goto 13
+# memory.write_byte(4, 8)
+# memory.write_byte(5, 13)
+# # X1 = X1 + memory[100]
+# memory.write_byte(6, 1)
+# memory.write_byte(7, 100)
+# # X1 = X1 + memory[100]
+# memory.write_byte(8, 1)
+# memory.write_byte(9, 100)
+# # memory[150] = X1
+# memory.write_byte(10, 5)
+# memory.write_byte(11, 150)
+# # halt
+# memory.write_byte(12, 255)
+# # X1 = X1 + memory[77]
+# memory.write_byte(13, 1)
+# memory.write_byte(14, 77)
+# # memory[150] = X1
+# memory.write_byte(15, 5)
+# memory.write_byte(16, 150)
+# # halt
+# memory.write_byte(17, 255)
+#
+# clock.start(cpu)
+#
+# print(memory.read_word(150))
 
-# memory[777] = X1
-memory.write_byte(12, 5)
-memory.write_byte(13, 222)
-
-# halt
-memory.write_byte(14, 255)
-
-clock.start(cpu)
-
-print(memory.read_word(150))
-print(memory.read_word(222))
-
-""" memory.write_word(50, 21)
+memory.write_word(50, 21)
 memory.write_word(100, 32)
 memory.write_word(130, 10)
 
 # X <- X + memory[50]
-memory.write_byte(1, 2)      # X <- X + memory...
-memory.write_byte(2, 50)     # ...[50]
+memory.write_byte(0, 1)      # X <- X + memory...
+memory.write_byte(1, 50)     # ...[50]
 
 # if X=0 goto 7
-memory.write_byte(3, 15)
-memory.write_byte(4, 7)
+memory.write_byte(2, 8)
+memory.write_byte(3, 6)
 
 # X <- X + memory[100]
-memory.write_byte(5, 2)      # X <- X + memory...
-memory.write_byte(6, 100)    # ...[100]
+memory.write_byte(4, 1)      # X <- X + memory...
+memory.write_byte(5, 100)    # ...[100]
 
 # X <- X - memory[130]
-memory.write_byte(7, 6)
-memory.write_byte(8, 130)
+memory.write_byte(6, 3)
+memory.write_byte(7, 130)
 
 # memory[150] = X
-memory.write_byte(9, 10)
-memory.write_byte(10, 150)
+memory.write_byte(8, 5)
+memory.write_byte(9, 150)
 
 # stop
-memory.write_byte(11, 255)
- """
+memory.write_byte(10, 255)
+
+# run
+clock.start(cpu)
+
+print(memory.read_word(150))
--- micro-architecture/firmware.py
+++ micro-architecture/firmware.py
@@ -2,12 +2,13 @@ from array import array
 
 TOTAL_SIZE = 512
 
+
 class Firmware:
     def __init__(self, total_size=TOTAL_SIZE):
         self.firmware = array('q', [0]) * total_size
 
     def set_instruction(self, position, instruction):
-        if position >= TOTAL_SIZE: 
+        if position >= TOTAL_SIZE:
             return
 
         self.firmware[position] = instruction
@@ -18,10 +19,10 @@ class Firmware:
 
         return self.firmware[position]
 
-# next address | jmp | alu | C | write read fetch | A | B | 
+
+# next address | jmp | alu | C | write read fetch | A | B |
 # 000000000_000_00000000_00000000_000_000_000
 #     9      3     8        8      3   3   3
-
 firmware = Firmware()
 
 # main: fetch; goto MBR1
@@ -30,39 +31,37 @@ firmware.set_instruction(0, 0b000000000_100_00000000_00000000_001_000_000)
 firmware.set_instruction(255, 0b000000000_000_00000000_00000000_000_000_000)
 
 # add: X1 = X1 + memory[address]
-## MAR <- MBR1; read; goto 2
+# MAR <- MBR1; read; goto 2
 firmware.set_instruction(1, 0b000000010_000_00010100_10000000_010_000_010)
-## X1 <- X1 + MDR; fetch; goto MBR1
+# X1 <- X1 + MDR; fetch; goto MBR1
 firmware.set_instruction(2, 0b000000000_100_00111100_00010000_001_100_001)
 
 # sub: X1 = X1 - memory[address]
-## MAR <- MBR1; read; goto 4
+# MAR <- MBR1; read; goto 4
 firmware.set_instruction(3, 0b000000100_000_00010100_10000000_010_000_010)
-## X1 <- X1 - MDR; fetch; goto MBR1
+# X1 <- X1 - MDR; fetch; goto MBR1
 firmware.set_instruction(4, 0b000000000_100_00111111_00010000_001_001_100)
 
 # memory[address] = X1
-## MAR <- MBR1; read; goto 6
-firmware.set_instruction(5, 0b000000110_000_00010100_10000000_010_000_010)
-## MDR <- X1; write; fetch; goto MBR1
+# MAR <- MBR1; goto 6
+firmware.set_instruction(5, 0b000000110_000_00010100_10000000_000_000_010)
+# MDR <- X1; write; fetch; goto MBR1
 firmware.set_instruction(6, 0b000000000_100_00010100_01000000_101_000_100)
 
-# goto address 
-## PC <- MBR1; fetch; goto MBR1
+# goto address
+# PC <- MBR1; fetch; goto MBR1
 firmware.set_instruction(7, 0b000000000_100_00010100_00100000_001_000_010)
 
-## 13: PC <- PC + 1; fetch; goto 14
-# firmware.set_instruction(13, 0b00001110_000_00110101_001000_001_001)
-
-## 14: PC <- MBR; fetch; goto MBR
-# firmware.set_instruction(14, 0b00000000_100_00010100_001000_001_010)
-
-
-
-# if X == 0 goto address
+# if X1 == 0 goto address
+# X1 <- X1; if alu = 0 goto 265 else goto 9
+firmware.set_instruction(8, 0b000001001_001_00010100_00010000_000_000_100)
+# fetch; goto MBR1
+firmware.set_instruction(9, 0b000000000_100_00000000_00000000_001_000_000)
+# goto address
+firmware.set_instruction(265, 0b000000000_100_00010100_00100000_001_000_010)
 
 """ # main: PC <- PC + 1; MBR <- read_byte(PC); goto MBR
-firmware.set_instruction(0, 0b000000000_100_00110101_001000_001_001) 
+firmware.set_instruction(0, 0b000000000_100_00110101_001000_001_001)
 
 # HALT
 firmware.set_instruction(255, 0b000000000_000_00000000_000000_000_000)
@@ -107,7 +106,7 @@ firmware.set_instruction(11, 0b00001100_000_00010100_100000_000_010)
 ## 12: MDR <- X; write; goto 0
 firmware.set_instruction(12, 0b00000000_000_00010100_010000_100_011)
 
-# goto address 
+# goto address
 
 ## 13: PC <- PC + 1; fetch; goto 14
 firmware.set_instruction(13, 0b00001110_000_00110101_001000_001_001)
