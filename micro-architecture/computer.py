0bf952f715429e66803981c342533f3a855cfa10
Gustavo Costa <gustavo1204.gc@gmail.com>
2023-06-25 23:00:22 -0300

    Save working changes

--- micro-architecture/computer.py
+++ micro-architecture/computer.py
@@ -2,73 +2,27 @@ from cpu import cpu
 from memory import memory
 from clock import clock
 
-# # memory[50] = 2
-# memory.write_word(50, 2)
-# # memory[100] = 2
-# memory.write_word(100, 2)
-# # memory[77] = 77
-# memory.write_word(77, 77)
-# # X1 <- X1 + memory[50]
-# memory.write_byte(0, 1)
-# memory.write_byte(1, 50)
-# # X1 <- X1 - memory[100]
-# memory.write_byte(2, 3)
-# memory.write_byte(3, 100)
-# # if X1 == 0 goto 13
-# memory.write_byte(4, 8)
-# memory.write_byte(5, 13)
-# # X1 = X1 + memory[100]
-# memory.write_byte(6, 1)
-# memory.write_byte(7, 100)
-# # X1 = X1 + memory[100]
-# memory.write_byte(8, 1)
-# memory.write_byte(9, 100)
-# # memory[150] = X1
-# memory.write_byte(10, 5)
-# memory.write_byte(11, 150)
-# # halt
-# memory.write_byte(12, 255)
-# # X1 = X1 + memory[77]
-# memory.write_byte(13, 1)
-# memory.write_byte(14, 77)
-# # memory[150] = X1
-# memory.write_byte(15, 5)
-# memory.write_byte(16, 150)
-# # halt
-# memory.write_byte(17, 255)
-#
-# clock.start(cpu)
-#
-# print(memory.read_word(150))
-
+# memory[50] = 20
 memory.write_word(50, 21)
-memory.write_word(100, 32)
-memory.write_word(130, 10)
+# memory[100] = 20
+memory.write_word(100, 22)
 
-# X <- X + memory[50]
-memory.write_byte(0, 1)      # X <- X + memory...
-memory.write_byte(1, 50)     # ...[50]
+# X1 <- X1 + memory[50]
+memory.write_byte(1, 1)
+memory.write_byte(2, 50)
+# X2 <- X2 + memory[100]
+memory.write_byte(3, 10)
+memory.write_byte(4, 100)
 
-# if X=0 goto 7
-memory.write_byte(2, 8)
-memory.write_byte(3, 6)
+# cmp X1, X2
+memory.write_byte(5, 26)
+# halt
+memory.write_byte(6, 255)
 
-# X <- X + memory[100]
-memory.write_byte(4, 1)      # X <- X + memory...
-memory.write_byte(5, 100)    # ...[100]
-
-# X <- X - memory[130]
-memory.write_byte(6, 3)
-memory.write_byte(7, 130)
-
-# memory[150] = X
-memory.write_byte(8, 5)
-memory.write_byte(9, 150)
-
-# stop
-memory.write_byte(10, 255)
-
-# run
 clock.start(cpu)
 
-print(memory.read_word(150))
+if cpu.registers["H"] == 0:
+    print("X2 > X1")
+if cpu.registers["H"] == 1:
+    print("X1 >= X2")
+
---
+++ micro-architecture/prog3.asm
@@ -0,0 +1,20 @@
+goto main
+wb 0
+
+o ww 0
+a ww 10
+b ww 5
+c ww 1
+
+main  add  x1, b # x1 = 5 -> 4 -> 3 -> 2 -> 1 -> 0
+      jz   x1, final 
+      add2 x2, a # x2 = 10 + 10 + 10 + 10 + 10
+
+      sub  x1, c # x1 = 4 -> 3 -> 2 -> 1 -> 0
+      mov  x1, b # b  = 4 -> 3 -> 2 -> 1 -> 0
+      sub  x1, b # x1 = 0
+      
+      goto main
+
+final mov2 x2, o
+      halt
